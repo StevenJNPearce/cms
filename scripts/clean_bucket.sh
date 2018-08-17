4a8ad03f7cd63e85180c9204b03aae4bcbb879cb
Eswara Sai <eswarasai.v@gmail.com>
2018-08-17 21:46:40 +0530

    Removed unnecessary assets

--- scripts/clean_bucket.sh
+++ scripts/clean_bucket.sh
@@ -7,14 +7,14 @@ then
   echo "Clearing Dev Bucket Prior To Deployment"
   eval export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
   eval export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
-  aws s3 rm s3://dev.blog.marketprotocol.io --recursive
+  aws s3 rm s3://dev.cms.marketprotocol.io --recursive
 elif [ "$TRAVIS_BRANCH" = "master" ]
 then
   echo "Invalidating CloudFront Cache"
   #aws configure set preview.cloudfront true
   #aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
   echo "Clearing Production Bucket Prior To Deployment (Leaving Discord and Telegram redirects)"
-  aws s3 rm s3://blog.marketprotocol.io --recursive
+  aws s3 rm s3://cms.marketprotocol.io --recursive
 else
   echo "No deployment on this branch"
 fi
--- src/assets/images/advisors/index.js
+++
@@ -1,8 +0,0 @@
-exports.BrentTraidman = require('./brent-traidman.svg');
-exports.BrianShields = require('./brian-shields.svg');
-exports.CasperJohansen = require('./casper-johansen.svg');
-exports.DanHorowitz = require('./dan-horowitz.svg');
-exports.JoshFraser = require('./josh-fraser.svg');
-exports.PatrickCharles = require('./patrick-charles.svg');
-exports.RobBehnke = require('./rob-behnke.png');
-exports.KevinOwocki = require('./kevin-owocki.svg');
