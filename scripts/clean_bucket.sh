31b7b38bba5f212d6648255e6b2acdbbc02ac043
Phil Elsasser <phil@marketprotocol.io>
2018-08-23 09:52:58 -0600

    modify clean up scripts to preserve redirects

--- scripts/clean_bucket.sh
+++ scripts/clean_bucket.sh
@@ -7,14 +7,14 @@ then
   echo "Clearing Dev Bucket Prior To Deployment"
   eval export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
   eval export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
-  aws s3 rm s3://dev.cms.marketprotocol.io --recursive
+  aws s3 rm s3://dev.cms.marketprotocol.io --recursive --exclude="redirectToBlog"
 elif [ "$TRAVIS_BRANCH" = "master" ]
 then
   echo "Invalidating CloudFront Cache"
   #aws configure set preview.cloudfront true
   #aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
-  echo "Clearing Production Bucket Prior To Deployment (Leaving Discord and Telegram redirects)"
-  aws s3 rm s3://cms.marketprotocol.io --recursive
+  echo "Clearing Production Bucket Prior To Deployment"
+  aws s3 rm s3://cms.marketprotocol.io --recursive --exclude="redirectToBlog"
 else
   echo "No deployment on this branch"
 fi
