0a9ec703e4f081586db81dc7841a93b20a6be013
Phil Elsasser <phil@marketprotocol.io>
2018-08-23 10:02:14 -0600

    update to allow direct routes to work

--- scripts/clean_bucket.sh
+++ scripts/clean_bucket.sh
@@ -7,14 +7,14 @@ then
   echo "Clearing Dev Bucket Prior To Deployment"
   eval export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
   eval export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
-  aws s3 rm s3://dev.cms.marketprotocol.io --recursive --exclude="redirectToBlog"
+  aws s3 rm s3://dev.cms.marketprotocol.io --recursive --exclude="index.html"
 elif [ "$TRAVIS_BRANCH" = "master" ]
 then
   echo "Invalidating CloudFront Cache"
   #aws configure set preview.cloudfront true
   #aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
   echo "Clearing Production Bucket Prior To Deployment"
-  aws s3 rm s3://cms.marketprotocol.io --recursive --exclude="redirectToBlog"
+  aws s3 rm s3://cms.marketprotocol.io --recursive --exclude="index.html"
 else
   echo "No deployment on this branch"
 fi
